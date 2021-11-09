35f936ffaa054760cb00493c041a5b569a44069f
Teresa Nguyen <teresanguyenpl@gmail.com>
2021-11-08 18:20:29 -0800

    created login and register class

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-const express = require("express")
+const express = require('express')
 const app = express()
 
-app.set("view engine", "ejs")
+app.set('view engine', 'ejs')
 
 app.get('/', (req, res)  => {
     console.log("Here")
