43b6ac6d7befbf9c3d5d8feb092091566ed66394
Teresa Nguyen <teresanguyenpl@gmail.com>
2021-11-08 18:29:37 -0800

    endpoint of register and login

--- server.js
+++ server.js
@@ -12,8 +12,16 @@ app.get('/login', (req, res) => {
     res.render('login.ejs')
 })
 
+app.post('/login', (req, res) => {
+   
+})
+
 app.get('/register', (req, res) => {
     res.render('register.ejs')
+})
+
+app.post('/register', (req, res) => {
+
 })
 
 
